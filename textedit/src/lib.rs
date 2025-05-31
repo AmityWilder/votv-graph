3eb6e7c3037c72ebf9534a4744aa7131887e366b
Amy Wilder <henrythepony@gmail.com>
2025-05-31 17:54:14 -0400

    Improve step

--- textedit/src/lib.rs
+++ textedit/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(char_max_len)]
+#![feature(char_max_len, variant_count, iterator_try_reduce)]
 
 use std::{clone, collections::VecDeque, num::NonZeroIsize, ops::Range, time::{Duration, Instant}};
 pub mod word;
@@ -12,11 +12,44 @@ pub enum Direction {
     Up,
 }
 
+impl Direction {
+    #[inline]
+    pub const fn is_vertical(self) -> bool {
+        (self as u8 & 2) != 0
+    }
+
+    #[inline]
+    pub const fn is_horizontal(self) -> bool {
+        !self.is_vertical()
+    }
+
+    #[inline]
+    pub const fn is_backward(self) -> bool {
+        (self as u8 & 1) != 0
+    }
+
+    #[inline]
+    pub const fn is_forward(self) -> bool {
+        !self.is_backward()
+    }
+}
+
+const _: () = {
+    assert!(Direction::Right.is_forward());
+    assert!(Direction::Right.is_horizontal());
+    assert!(Direction::Left.is_backward());
+    assert!(Direction::Left.is_horizontal());
+    assert!(Direction::Down.is_forward());
+    assert!(Direction::Down.is_vertical());
+    assert!(Direction::Up.is_backward());
+    assert!(Direction::Up.is_vertical());
+};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub enum CursorStep {
     /// column/row
     #[default]
-    Character,
+    Single,
     /// word/page
     Segment,
     /// line/document
@@ -24,28 +57,153 @@ pub enum CursorStep {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct MoveCursor {
-    /// In what direction the step is taken
-    pub direction: Direction,
-    /// Whether the movement should clear the selection and reposition
-    /// instead of adding to/removing from the selection
-    pub is_selecting: bool,
-    /// How far the cursor should move
-    pub step_by: CursorStep,
+pub struct MoveCursor(u8);
+
+impl MoveCursor {
+    const STEP_BY_OFFSET: u32 = u32::MIN;
+
+    const STEP_BY_BITS: u8 =
+        CursorStep::Single as u8 |
+        CursorStep::Segment as u8 |
+        CursorStep::Full as u8;
+
+    const STEP_BY_MASK: u8 = Self::STEP_BY_BITS << Self::STEP_BY_OFFSET;
+
+    const DIRECTION_OFFSET: u32 =
+        Self::STEP_BY_MASK
+            .next_power_of_two()
+            .trailing_zeros();
+
+    const DIRECTION_BITS: u8 =
+        Direction::Right as u8 |
+        Direction::Left as u8 |
+        Direction::Down as u8 |
+        Direction::Up as u8;
+
+    const DIRECTION_MASK: u8 = Self::DIRECTION_BITS << Self::DIRECTION_OFFSET;
+
+    const IS_SELECTING_OFFSET: u32 =
+        Self::DIRECTION_MASK
+            .next_power_of_two()
+            .trailing_zeros();
+
+    const IS_SELECTING_BITS: u8 =
+        false as u8 |
+        true as u8;
+
+    const IS_SELECTING_MASK: u8 = Self::IS_SELECTING_BITS << Self::IS_SELECTING_OFFSET;
+
+    #[inline]
+    pub const fn new(step_by: CursorStep, direction: Direction, is_selecting: bool) -> Self {
+        Self(
+            ((is_selecting as u8) << Self::IS_SELECTING_OFFSET) |
+            ((direction as u8) << Self::DIRECTION_OFFSET) |
+            ((step_by as u8) << Self::STEP_BY_OFFSET)
+        )
+    }
+
+    #[inline]
+    pub const fn step_by(self) -> CursorStep {
+        unsafe { std::mem::transmute(self.0 & Self::STEP_BY_MASK) }
+    }
+
+    #[inline]
+    pub const fn direction(self) -> Direction {
+        unsafe { std::mem::transmute((self.0 & Self::DIRECTION_MASK) >> 2) }
+    }
+
+    #[inline]
+    pub const fn is_selecting(self) -> bool {
+        (self.0 & Self::IS_SELECTING_MASK) != 0
+    }
+
+    #[inline]
+    pub fn apply(&self, string: &str, page_lines: usize, pos: usize) -> usize {
+        assert!(pos <= string.len());
+
+        let dir = self.direction();
+        let step = self.step_by();
+
+        let size = {
+            let within = if dir.is_forward() { &string[pos..] } else { &string[..pos] };
+
+            match (step, dir) {
+                (CursorStep::Full, Direction::Down | Direction::Up)  => None,
+
+                (CursorStep::Single, Direction::Right | Direction::Left) => {
+                    let mut it = within.chars().map(|ch| ch.len_utf8());
+                    let next = if dir.is_forward() { Iterator::next } else { DoubleEndedIterator::next_back };
+                    next(&mut it)
+
+                }
+
+                (CursorStep::Segment, Direction::Right | Direction::Left) => {
+                    let mut it = within.words().map(|s| s.len());
+                    let next = if dir.is_forward() { Iterator::next } else { DoubleEndedIterator::next_back };
+                    next(&mut it)
+
+                }
+
+                (CursorStep::Full, Direction::Right | Direction::Left) => {
+                    let mut it = within.lines().map(|s| s.len());
+                    let next = if dir.is_forward() { Iterator::next } else { DoubleEndedIterator::next_back };
+                    next(&mut it)
+
+                }
+
+                (CursorStep::Single | CursorStep::Segment, Direction::Down | Direction::Up) => {
+                    // in chars, not bytes
+                    let col = string[..pos].chars().rev()
+                        .position(|ch| ch == '\n')
+                        .unwrap_or(pos);
+
+                    let mut it = within.lines().map(|s| s.len());
+                    let next = if dir.is_forward() { Iterator::next } else { DoubleEndedIterator::next_back };
+
+                    let n = match step {
+                        CursorStep::Single => 1,
+                        CursorStep::Segment => page_lines,
+                        CursorStep::Full => unreachable!(),
+                    };
+
+                    (0..n)
+                        .map(|_| next(&mut it))
+                        .try_fold(col, |acc, item| {
+                            item.map(|x| acc + x)
+                        })
+                        .and_then(|len| within[len..]
+                            .char_indices()
+                            .nth(col)
+                            .map(|(i, _)| i)
+                        )
+                }
+            }.unwrap_or_else(|| within.len())
+        };
+
+        if dir.is_forward() { pos + size } else { pos - size }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Input {
+    /// A single unicode character (shift/caps should be externally accounted)
     Symbol(char),
+    /// Modify the cursor position relative to where it is currently
     MoveCursor(MoveCursor),
+    /// Set the selection to the entire document
     SelectAll,
+    /// Copy the selection to the clipboard and erase the selected text
     Cut,
+    /// Copy the selection to the clipboard
     Copy,
+    /// Paste the clipboard over the selection
     Paste,
     /// Set the selection to only the head
     Deselect,
-    DeleteBefore,
-    DeleteAfter,
+    /// Erase a step to the left
+    DeleteBefore(CursorStep),
+    /// Erase a step to the right
+    DeleteAfter(CursorStep),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -140,6 +298,7 @@ impl Editor {
         &mut self,
         input: Input,
         string: &mut String,
+        page_lines: usize,
         clipboard: &mut C,
     ) -> Option<Edit> {
         let selection = self.selection_range();
@@ -149,42 +308,10 @@ impl Editor {
         }
 
         match &input {
-            Input::MoveCursor(m) => {
-                self.selection_tail = match (m.step_by, m.direction) {
-                    (CursorStep::Full, Direction::Down) => string.len(),
-                    (CursorStep::Full, Direction::Up) => 0,
-                    _ => {
-                        let within = match m.direction {
-                            Direction::Right | Direction::Down => &string[self.selection_tail..],
-                            Direction::Left  | Direction::Up   => &string[..self.selection_tail],
-                        };
-                        match m.direction {
-                            Direction::Right => match m.step_by {
-                                CursorStep::Character => todo!(),
-                                CursorStep::Segment => todo!(),
-                                CursorStep::Full => todo!(),
-                            }
-                            Direction::Down => match m.step_by {
-                                CursorStep::Character => todo!(),
-                                CursorStep::Segment => todo!(),
-                                CursorStep::Full => todo!(),
-                            }
-                            Direction::Left => match m.step_by {
-                                CursorStep::Character => todo!(),
-                                CursorStep::Segment => todo!(),
-                                CursorStep::Full => todo!(),
-                            }
-                            Direction::Up => match m.step_by {
-                                CursorStep::Character => todo!(),
-                                CursorStep::Segment => todo!(),
-                                CursorStep::Full => todo!(),
-                            }
-                        }
-                        todo!()
-                    }
-                };
+            Input::MoveCursor(movement) => {
+                self.selection_tail = movement.apply(string, page_lines, self.selection_tail);
 
-                if !m.is_selecting {
+                if !movement.is_selecting() {
                     self.selection_head = self.selection_tail;
                 }
             }
@@ -201,8 +328,8 @@ impl Editor {
             | Input::Symbol(_)
             | Input::Cut
             | Input::Paste
-            | Input::DeleteBefore
-            | Input::DeleteAfter
+            | Input::DeleteBefore(_)
+            | Input::DeleteAfter(_)
                 => {
                     let mut buf;
                     let insertion = match input {
@@ -214,8 +341,8 @@ impl Editor {
                         Input::Paste => clipboard.get().unwrap_or(""),
 
                         | Input::Cut
-                        | Input::DeleteBefore
-                        | Input::DeleteAfter
+                        | Input::DeleteBefore(_)
+                        | Input::DeleteAfter(_)
                             => "",
 
                         _ => unreachable!(),
@@ -234,13 +361,14 @@ impl Editor {
         input: InputEvent,
         last_update: &Instant,
         rep: &Repeater,
+        page_lines: usize,
         string: &mut String,
         clipboard: &mut C,
     ) -> Vec<Edit> {
         match (input, &self.held_input) {
             (InputEvent::Update, &Some(HeldInput { input, start_time })) => {
                 let n = rep.count(last_update.duration_since(start_time));
-                return std::iter::repeat_with(|| self.press(input, string, clipboard))
+                return std::iter::repeat_with(|| self.press(input, string, page_lines, clipboard))
                     .take(n)
                     .filter_map(|x| x)
                     .collect();
@@ -248,7 +376,7 @@ impl Editor {
 
             (InputEvent::Press(input), _) => {
                 self.held_input = Some(HeldInput::now(input));
-                return self.press(input, string, clipboard)
+                return self.press(input, string, page_lines, clipboard)
                     .as_slice()
                     .to_vec();
             }
@@ -261,5 +389,45 @@ impl Editor {
         };
 
         Vec::new()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const ASCII_TEST_STR: &str = "apple orange\nbanana mango\nstrawberry kiwi\npineapple";
+    const UTF8_TEST_STR: &str = "appl𒉌e oran௵ge\nbana𒎔na manʩgo\nstr𒈝awஹberry𒈼 kiwi\npine𒈍app௹le";
+
+    #[test]
+    fn test_len_ascii_single_right() {
+        let s = ASCII_TEST_STR;
+        for (head, expect) in [
+            (0, 1),             // start
+            (6, 7),             // arbitrary (within first line)
+            (12, 13),           // line boundary
+            (6, 7),             // arbitrary (within arbitrary line)
+            (6, 7),             // arbitrary (within last line)
+            (s.len(), s.len()), // document boundary
+        ] {
+            let tail = MoveCursor::new(CursorStep::Single, Direction::Right, true).apply(s, 1, head);
+            assert_eq!(tail, expect);
+        }
+    }
+
+    #[test]
+    fn test_len_ascii_single_left() {
+        let s = ASCII_TEST_STR;
+        for (head, expect) in [
+            (s.len(), s.len() - 1), // end
+            (7, 6),                 // arbitrary (within first line)
+            (13, 12),               // line boundary
+            (7, 6),                 // arbitrary (within arbitrary line)
+            (7, 6),                 // arbitrary (within last line)
+            (0, 0),                 // document boundary
+        ] {
+            let tail = MoveCursor::new(CursorStep::Single, Direction::Left, true).apply(s, 1, head);
+            assert_eq!(tail, expect);
+        }
     }
 }
